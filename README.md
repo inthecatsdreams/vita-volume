ce40e52648ff58bbd9281a6790f36df989130402
inthecatsdreams <inthecatsdreams@yandex.ru>
2019-08-01 15:06:01 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# v1.1   
+Fixed issue where Game Updates couldnt  be downloaded if "Block Update Download" was enabled   
+(https://www.reddit.com/r/VitaPiracy/comments/ajibp7/cant_update_from_livearea/)   
+
+Download (VPK):  
+https://bitbucket.org/SilicaAndPina/updateblocker/downloads/UpdateBlocker-1.1.vpk  
+
+# UpdateBlocker    
+
+Features:   
+  
+1) Block "System Update" option in settings    
+when this is enables the System Update tab in Settings wont allow you to complete an update.    
+essentally blocking updates complete from here    
+   
+   
+2) Block Update Downloads    
+When this is enabled, update files cannot be automatically downloaded by the system     
+(even if its enabled in auto-start settings) - the auto-start setting for system updates is also turned off. .    
+
+
+Once enabled these features will work even without henkaku enabled.
+
+The "Block Update Download" will be disabled upon restoring the system however,
+and "Block System Update In Settings" will be disabled upon reinstalling the firmware.
+
+In both cases, you can still use the update function in safe mode.   
+so if you ever semi-brick its still recoverable...     
+  
+Download (VPK):  
+https://bitbucket.org/SilicaAndPina/updateblocker/downloads/UpdateBlocker-1.1.vpk
+
---
+++ src/main.c
@@ -0,0 +1,81 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <vitasdk.h>
+
+#include "ctrl.h"
+#include "debugScreen.h"
+
+#define printf psvDebugScreenPrintf
+int ret;
+
+
+
+void increaseVolume(int vol){
+	psvDebugScreenClear();
+	if (vol == 30){
+		psvDebugScreenPrintf("You are alredy at the maxium.");
+		sceKernelDelayThread(1000000);
+		main();
+	}
+	else {
+		int ret  = sceRegMgrSetKeyInt("/CONFIG/SOUND/", "main_volume", vol + 1);
+		printf("Volume has been increased.");
+		sceKernelDelayThread(1000000);
+		main();
+	}
+}
+int getCurrentVolume(){
+	int val = 0;
+	int buff = sceRegMgrGetKeyInt("/CONFIG/SOUND/", "main_volume", &val);
+	return val;
+}
+
+void decreaseVolume(int vol){
+	psvDebugScreenClear();
+	if (vol == 5){
+		psvDebugScreenPrintf("You are alredy at the minimum.");
+		sceKernelDelayThread(1000000);
+		main();
+	}
+	else {
+		int ret  = sceRegMgrSetKeyInt("/CONFIG/SOUND/", "main_volume", vol - 1);
+		printf("Volume has been decreased.");
+		sceKernelDelayThread(1000000);
+		main();
+	}
+}
+
+int main() {
+		psvDebugScreenInit();
+        psvDebugScreenClear(0);
+		psvDebugScreenPrintf("--- Vita Volume by inthecatsdreams ---\n");
+        psvDebugScreenPrintf("CROSS: Increase Volume\n");
+        psvDebugScreenPrintf("CIRCLE: Decrease Volume\n");
+        psvDebugScreenPrintf("SQUARE: Apply settings and reboot\n");
+		psvDebugScreenPrintf("Current Volume: %d\n", getCurrentVolume());
+		int currentVolume = getCurrentVolume();
+		
+        
+        sceKernelDelayThread(100000);
+        while(1)
+        {
+        switch(get_key(0)) {
+                case SCE_CTRL_CROSS:
+                    increaseVolume(currentVolume);
+                    break;
+                case SCE_CTRL_CIRCLE:
+                    decreaseVolume(currentVolume);
+                    break;
+                case SCE_CTRL_SQUARE:
+					scePowerRequestColdReset();
+                    break;
+                default:
+                    break;
+                }
+        }
+        
+
+        
+        return 0;
+}
