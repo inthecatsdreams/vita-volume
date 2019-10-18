cfcb95426f8938983f8240abf5de73583a1c98a4
2501-Makoto <saitei@disroot.org>
2019-10-18 16:59:32 +0200

    cleaned the code a bit

--- src/main.c
+++ src/main.c
@@ -7,84 +7,70 @@
 #include "debugScreen.h"
 
 #define printf psvDebugScreenPrintf
+#define clearScreen psvDebugScreenClear
 int ret;
 
-void
-increaseVolume (int vol)
-{
-  psvDebugScreenClear ();
-  if (vol == 30)
-    {
-      psvDebugScreenPrintf ("You are alredy at the maxium.");
-      main ();
+void increaseVolume(int vol){
+  clearScreen();
+  if (vol == 30){
+      printf("You are alredy at the maxium. Nothing to do.");
+      main();
     }
-  else
-    {
-      int ret = sceRegMgrSetKeyInt ("/CONFIG/SOUND/", "main_volume", vol + 1);
-      main ();
+  else {
+      int ret = sceRegMgrSetKeyInt("/CONFIG/SOUND/", "main_volume", vol + 1);
+      main();
     }
 }
 
-int
-getCurrentVolume ()
-{
+int getCurrentVolume(){
   int val = 0;
-  int buff = sceRegMgrGetKeyInt ("/CONFIG/SOUND/", "main_volume", &val);
+  int buff = sceRegMgrGetKeyInt("/CONFIG/SOUND/", "main_volume", &val);
   return val;
 }
 
-void
-muteConsole()
-{
-  psvDebugScreenClear();
-  int ret = sceRegMgrSetKeyInt ("/CONFIG/SOUND/","main_volume", 0);
-  main ();
+void muteConsole(){
+  clearScreen();
+  int ret = sceRegMgrSetKeyInt("/CONFIG/SOUND/","main_volume", 0);
+  main();
 
 }
 
-void
-decreaseVolume (int vol)
-{
-  psvDebugScreenClear ();
-  if (vol == 0)
-    {
-      psvDebugScreenPrintf ("You are alredy at the minimum.");
-      main ();
+void decreaseVolume(int vol){
+  clearScreen();
+  if (vol == 0){
+      printf("You are alredy at the minimum.");
+      main();
     }
-  else
-    {
+  else {
       int ret = sceRegMgrSetKeyInt ("/CONFIG/SOUND/", "main_volume", vol - 1);
       printf ("Volume has been decreased.");
-      main ();
+      main();
     }
 }
 
-int
-main ()
+int main()
 {
   psvDebugScreenInit ();
-  psvDebugScreenClear (0);
-  psvDebugScreenPrintf ("--- Vita Volume by inthecatsdreams ---\n");
-  psvDebugScreenPrintf ("CROSS: Increase Volume\n");
-  psvDebugScreenPrintf ("CIRCLE: Decrease Volume\n");
-  psvDebugScreenPrintf ("TRIANGLE: Mute Console\n");
-  psvDebugScreenPrintf ("SQUARE: Apply settings and reboot\n");
-  psvDebugScreenPrintf ("Current Volume: %d\n", getCurrentVolume ());
-  int currentVolume = getCurrentVolume ();
-  sceKernelDelayThread (100000);
-  while (1)
-    {
+  clearScreen(0);
+  printf("--- Vita Volume by inthecatsdreams ---\n");
+  printf("CROSS: Increase Volume\n");
+  printf("CIRCLE: Decrease Volume\n");
+  printf("TRIANGLE: Mute Console\n");
+  printf("SQUARE: Apply settings and reboot\n");
+  printf("Current Volume: %d\n", getCurrentVolume());
+  int currentVolume = getCurrentVolume();
+  sceKernelDelayThread(100000);
+  while(1){
 
-      switch (get_key (0))
-	{
+      switch(get_key (0)){
 	case SCE_CTRL_CROSS:
-	  increaseVolume (currentVolume);
+	  increaseVolume(currentVolume);
 	  break;
 	case SCE_CTRL_CIRCLE:
-	  decreaseVolume (currentVolume);
+	  decreaseVolume(currentVolume);
 	  break;
 	case SCE_CTRL_SQUARE:
-	  scePowerRequestColdReset ();
+	  scePowerRequestColdReset();
 	  break;
 	case SCE_CTRL_TRIANGLE:
 	  muteConsole();
