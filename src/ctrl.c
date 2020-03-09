953979701ba2f69f47ffae7d312d7c2d500f47d3
ic92 <inthecatsdreams@yandex.ru>
2020-03-09 09:25:01 +0100

    1.4.0

--- src/ctrl.c
+++ src/ctrl.c
@@ -5,64 +5,54 @@
 
 #define printf psvDebugScreenPrintf
 
-int
-get_key (int type)
+int get_key(int type)
 {
 
+	SceCtrlData pad;
 
-  SceCtrlData pad;
-
-  if (type == 0)
-    {
-
-      while (1)
-	{
-	  //memset(&pad, 0, sizeof(pad));
-	  sceCtrlPeekBufferPositive (0, &pad, 1);
-
-	  if (pad.buttons != 0)
-	    return pad.buttons;
-
-	  sceKernelDelayThread (1000);	// 1ms
-	}
-
-    }
-  else
-    {
-
-      while (1)
+	if (type == 0)
 	{
 
-	  //memset(&pad, 0, sizeof(pad));
-	  sceCtrlPeekBufferPositive (0, &pad, 1);
-	  if (pad.buttons == 0)
-	    {
+		while (1)
+		{
+			//memset(&pad, 0, sizeof(pad));
+			sceCtrlPeekBufferPositive(0, &pad, 1);
 
-	      break;
+			if (pad.buttons != 0)
+				return pad.buttons;
 
-	    }
+			sceKernelDelayThread(1000); // 1ms
+		}
+	}
+	else
+	{
 
-	  sceKernelDelayThread (1000);	// 1ms
+		while (1)
+		{
 
+			//memset(&pad, 0, sizeof(pad));
+			sceCtrlPeekBufferPositive(0, &pad, 1);
+			if (pad.buttons == 0)
+			{
+
+				break;
+			}
+
+			sceKernelDelayThread(1000); // 1ms
+		}
 	}
 
-    }
-
-  return 0;
-
+	return 0;
 }
 
-
-void
-press_exit (void)
+void press_exit(void)
 {
 
-  get_key (1);
+	get_key(1);
 
-  printf ("Press any key to exit this application.\n");
+	printf("Press any key to exit this application.\n");
 
-  get_key (0);
+	get_key(0);
 
-  sceKernelExitProcess (0);
-
+	sceKernelExitProcess(0);
 }
--- src/main.c
+++ src/main.c
@@ -10,75 +10,84 @@
 #define clearScreen psvDebugScreenClear
 int ret;
 
-void increaseVolume(int vol){
+void increaseVolume(int vol)
+{
   clearScreen();
-  if (vol == 30){
-      printf("You are alredy at the maxium. Nothing to do.");
-      main();
-    }
-  else {
-      int ret = sceRegMgrSetKeyInt("/CONFIG/SOUND/", "main_volume", vol + 1);
-      main();
-    }
+  if (vol == 30)
+  {
+    printf("You are alredy at the maxium. Nothing to do.");
+    main();
+  }
+  else
+  {
+    int ret = sceRegMgrSetKeyInt("/CONFIG/SOUND/", "main_volume", vol + 1);
+    main();
+  }
 }
 
-int getCurrentVolume(){
+int getCurrentVolume()
+{
   int val = 0;
   int buff = sceRegMgrGetKeyInt("/CONFIG/SOUND/", "main_volume", &val);
   return val;
 }
 
-void muteConsole(){
+void muteConsole()
+{
   clearScreen();
-  int ret = sceRegMgrSetKeyInt("/CONFIG/SOUND/","main_volume", 0);
+  int ret = sceRegMgrSetKeyInt("/CONFIG/SOUND/", "main_volume", 0);
   main();
-
 }
 
-void decreaseVolume(int vol){
+void decreaseVolume(int vol)
+{
   clearScreen();
-  if (vol == 0){
-      printf("You are alredy at the minimum.");
-      main();
-    }
-  else {
-      int ret = sceRegMgrSetKeyInt ("/CONFIG/SOUND/", "main_volume", vol - 1);
-      printf ("Volume has been decreased.");
-      main();
-    }
+  if (vol == 0)
+  {
+    printf("You are alredy at the minimum.");
+    main();
+  }
+  else
+  {
+    int ret = sceRegMgrSetKeyInt("/CONFIG/SOUND/", "main_volume", vol - 1);
+    printf("Volume has been decreased.");
+    main();
+  }
 }
 
 int main()
 {
-  psvDebugScreenInit ();
+  psvDebugScreenInit();
   clearScreen(0);
-  printf("--- Vita Volume by inthecatsdreams ---\n");
-  printf("CROSS: Increase Volume\n");
-  printf("CIRCLE: Decrease Volume\n");
+  printf("--- Vita Volume by inthecatsdreams(ic92) ---\n");
+  printf("ARROW-UP: Increase Volume\n");
+  printf("ARROW-DOWN: Decrease Volume\n");
   printf("TRIANGLE: Mute Console\n");
-  printf("SQUARE: Apply settings and reboot\n");
+  printf("CROSS: Apply settings and reboot\n");
   printf("Current Volume: %d\n", getCurrentVolume());
   int currentVolume = getCurrentVolume();
   sceKernelDelayThread(100000);
-  while(1){
+  while (1)
+  {
 
-      switch(get_key (0)){
-	case SCE_CTRL_CROSS:
-	  increaseVolume(currentVolume);
-	  break;
-	case SCE_CTRL_CIRCLE:
-	  decreaseVolume(currentVolume);
-	  break;
-	case SCE_CTRL_SQUARE:
-	  scePowerRequestColdReset();
-	  break;
-	case SCE_CTRL_TRIANGLE:
-	  muteConsole();
-	  break;
-	default:
-	  break;
-	}
+    switch (get_key(0))
+    {
+    case SCE_CTRL_UP:
+      increaseVolume(currentVolume);
+      break;
+    case SCE_CTRL_DOWN:
+      decreaseVolume(currentVolume);
+      break;
+    case SCE_CTRL_CROSS:
+      scePowerRequestColdReset();
+      break;
+    case SCE_CTRL_TRIANGLE:
+      muteConsole();
+      break;
+    default:
+      break;
     }
+  }
 
   return 0;
 }
